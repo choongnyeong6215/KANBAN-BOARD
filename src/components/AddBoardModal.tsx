12922cc36b50382061d22ac02d7fd717ab1089a0
choongnyeong6215 <choong6215@gmail.com>
2024-03-20 17:47:51 +0900

    보드 추가 로직 작성

--- src/components/AddBoardModal.tsx
+++ src/components/AddBoardModal.tsx
@@ -1,5 +1,4 @@
-import Modal from "react-modal";
-import { boardModal } from "../recoil/atom";
+import { boardModal, toDoState } from "../recoil/atom";
 import { useRecoilState } from "recoil";
 import {
   ModalHeader,
@@ -11,11 +10,34 @@ import {
 // react-icons
 import { IoIosClose } from "react-icons/io";
 import { FaCirclePlus } from "react-icons/fa6";
+import { useState } from "react";
 
 const AddBoardModal = () => {
+  // 카테고리 목록
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
+  // 보드 추가 모달
   const [modalOpen, setModalOpen] = useRecoilState(boardModal);
 
   const handleCloseModal = () => setModalOpen(!modalOpen);
+
+  // 보드 추가 로직
+  const [newBoardName, setNewBoardName] = useState("");
+
+  const hanldeBoardName = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    setNewBoardName(e.currentTarget.value);
+  };
+
+  const handleSubmit = () => {
+    setToDos((prevToDos) => {
+      const newCategory = newBoardName;
+      return {
+        ...prevToDos,
+        [newCategory]: [],
+      };
+    });
+  };
 
   return (
     <StyledModal isOpen={modalOpen}>
@@ -25,8 +47,8 @@ const AddBoardModal = () => {
       <ModalTitle>
         <h1>생성할 보드 이름을 입력해주세요.</h1>
       </ModalTitle>
-      <AddBoardForm>
-        <input placeholder="asd" />
+      <AddBoardForm onSubmit={handleSubmit}>
+        <input type="text" value={newBoardName} onChange={hanldeBoardName} />
         <FaCirclePlus className="addModalBtn" />
       </AddBoardForm>
     </StyledModal>
