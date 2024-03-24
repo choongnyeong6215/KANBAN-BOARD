355fe610529e196a6acba6dec50b61dfd6fa86e3
choongnyeong6215 <choong6215@gmail.com>
2024-03-24 18:39:14 +0900

    스타일 개선

--- src/styles/dndStyle.ts
+++ src/styles/dndStyle.ts
@@ -1,121 +1,6 @@
 import styled from "styled-components";
 
-export const BoardContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 1000px;
-  margin: 2rem auto;
-`;
-
-export const BoardGroup = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 100%;
-  gap: 2rem;
-`;
-
-// Droppable
-export const Board = styled.div`
-  background-color: ${(props) => props.theme.boardColor};
-  padding: 2rem 1rem;
-  border-radius: 1rem;
-  display: flex;
-  flex-direction: column;
-  min-height: 300px;
-  .deletBoard {
-    display: none;
-  }
-  &:hover {
-    .deletBoard {
-      display: block;
-    }
-  }
-`;
-
-// Draggable
-export const Card = styled.div<{ $isDragging: boolean }>`
-  // 드래그 요소 색상 강조 처리
-  background-color: ${(props) => props.theme.cardColor};
-  padding: 1rem;
-  margin: 1rem 0;
-  border-radius: 1rem;
-`;
-
-export const BoardHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  aligin-items: center;
-  .deletBoard {
-    cursor: pointer;
-  }
-`;
-
-export const BoardTitle = styled.p`
-  text-align: center;
-  font-weight: bold;
-  font-size: 1.25rem;
-  padding-bottom: 1rem;
-`;
-
-// 드래그 요소 전체 영역
-export const DragArea = styled.div<{
-  $isDraggingOver: boolean;
-  $DraggingFromThisWith: boolean;
-}>`
-  // 리스트 드롭 시 해당 드롭 영역 색상 변경
-  background-color: ${(props) =>
-    props.$isDraggingOver ? "royalblue" : "transparent"};
-  border-radius: 1rem;
-  padding: 1rem;
-  flex-grow: 1;
-`;
-
-export const TodoForm = styled.form`
-  margin: 1em 0;
-  position: relative;
-  input {
-    padding: 1rem 1rem;
-    width: 100%;
-    border: none;
-    border-radius: 0.5rem;
-    background-color: ${(props) => props.theme.cardColor};
-    color: ${(props) => props.theme.fontColor};
-  }
-  button {
-    position: absolute;
-    right: 0;
-    top: 0;
-    height: 100%;
-    padding: 0.5rem;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    color: black;
-    font-weight: bold;
-  }
-`;
-
-export const TrashCan = styled.div<{ $isDraggingOver: boolean }>`
-  position: fixed;
-  bottom: 1rem;
-  right: 1rem;
-  font-size: 3rem;
-  padding: 1rem;
-  background-color: ${(props) =>
-    props.$isDraggingOver ? "tomato" : "transparent"};
-  width: 5rem;
-  border-radius: 2.5rem;
-  .logo {
-    color: ${(props) => props.theme.iconColor};
-    cursor: pointer;
-  }
-  &:hover {
-    transform: scale(1.1);
-    transition: transform 0.3s;
-  }
-`;
-
+// DragDropContext
 export const Header = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -138,13 +23,192 @@ export const ModeBtn = styled.button`
   background-color: transparent;
 `;
 
+export const BoardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 1200px;
+  margin: 5rem auto;
+`;
+
+export const BoardGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 100%;
+  gap: 2rem;
+`;
+
+export const TrashCan = styled.div<{ $isDraggingOver: boolean }>`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  font-size: 3rem;
+  padding: 1rem;
+  background-color: ${(props) =>
+    props.$isDraggingOver ? "tomato" : "transparent"};
+  width: 5rem;
+  border-radius: 2.5rem;
+  .logo {
+    color: ${(props) => props.theme.iconColor};
+    cursor: pointer;
+  }
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.3s;
+  }
+`;
+
+// Droppable
+export const Board = styled.div`
+  background-color: ${(props) => props.theme.boardColor};
+  padding: 2rem 1rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  min-height: 300px;
+  .deletBoard {
+    display: none;
+  }
+  &:hover {
+    .deletBoard {
+      display: block;
+    }
+  }
+`;
+
+export const BoardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  .deletBoard {
+    cursor: pointer;
+    font-size: 1.25rem;
+  }
+`;
+
+export const BoardTitle = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.5rem;
+`;
+
+export const TodoForm = styled.form`
+  margin: 1em 0;
+  position: relative;
+  input {
+    padding: 1rem;
+    width: 100%;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: ${(props) => props.theme.cardColor};
+    color: ${(props) => props.theme.fontColor};
+  }
+`;
+
 export const AddTaskBtn = styled.button`
-  .btnIcon {
+  position: absolute;
+  top: 0;
+  right: 1rem;
+  height: 100%;
+  font-size: 1.25rem;
+  border: none;
+  background-color: transparent;
+  color: royalblue;
+`;
+
+// 드래그 요소 전체 영역
+export const DragArea = styled.div<{
+  $isDraggingOver: boolean;
+  $DraggingFromThisWith: boolean;
+}>`
+  // 리스트 드롭 시 해당 드롭 영역 색상 변경
+  background-color: ${(props) =>
+    props.$isDraggingOver ? "royalblue" : "transparent"};
+  border-radius: 1rem;
+  padding: 1rem;
+  flex-grow: 1;
+`;
+
+// Draggable
+export const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  padding: 1rem;
+  margin-bottom: 1.5rem;
+  border-radius: 1rem;
+`;
+
+export const ToDoSection = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & .editBtn {
+    display: none;
+  }
+  &:hover {
+    .editBtn {
+      display: block;
+    }
+  }
+`;
+
+export const EditForm = styled.form<{ $isEdit: boolean }>`
+  display: flex;
+  justifyContent: space-between;
+  alignItems: center;
+  input {
+    position: relative;
+    padding: 1rem;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: transparent;
+    color: ${(props) => props.theme.fontColor};
+    font-size: 0.8rem;
+  }
+  button {
     position: absolute;
     top: 0;
-    right: 0.5rem;
+    right: 2rem;
     height: 100%;
-    font-size: 1.5rem;
-    color: royalblue;
+    display: ${(props) => (props.$isEdit ? "block" : "none")};
+  }
+}
+`;
+
+export const ConFirmEditBtn = styled.button<{ $isEdit: boolean }>`
+  border: none;
+  background-color: transparent;
+`;
+
+export const EditBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  color: ${(props) => props.theme.fontColor};
+  .editIcon {
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+`;
+
+export const CancelBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  color: ${(props) => props.theme.fontColor};
+  .cancelIcon {
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+`;
+
+export const ConfirmBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  color: ${(props) => props.theme.fontColor};
+  .confirmIcon {
+    font-size: 1.25rem;
+    cursor: pointer;
+    top: 0;
+    right: 0;
   }
 `;
